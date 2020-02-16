88e168140b32877835366977999582d2912b406a
Pawel Kaliszewski <kaliszewskipawel@o2.pl>
2020-02-16 13:51:24 +0100

    initial commit

---
+++ app/src/main/java/com/example/yourweather/activities/HomeScreen.kt
@@ -0,0 +1,13 @@
+package com.example.yourweather.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.yourweather.R
+
+class HomeScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
